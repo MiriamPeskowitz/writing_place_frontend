86c4a192dcf487eb4f1274dd9f13da55652790ec
MiriamPeskowitz <miriam5000@gmail.com>
2020-08-05 00:30:36 -0400

    Add fetchSites, first GET request, works

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log("js working")
+const endPoint = "http://localhost:3000/api/v1/sites"
+
+document.addEventListener('DOMContentLoaded', () => {
+	console.log('loaded')
+	fetchSites()
+	
+})
+
+function fetchSites() {
+	fetch(endPoint)
+ 	 .then(response => response.json())
+ 	 .then(sites => {
+ 	 	console.log("sites: ", sites )
+ 	 })
+ 	 // add catch
+}
